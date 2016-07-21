76675b48559c7f348d4a3f47e256fcc9e3b498b5
Nayan <nayan.s@prodigan.com>
2016-07-21 15:51:35 +0530

    created employee

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -2,14 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests;
+use Illuminate\Http\Request;
+
+use Auth;
+use Input;
+use Session;
+use Carbon;
+use Lang;
+use Validator;
+use URL;
+use DB;
+use Timezone;
+use stdClass;
+use Response;
+use DateTimeZone;
+use DateTime;
+
+
+
 Class EmployeeController extends Controller {
 
 	Public function getIndex()
 	{
 		return view('employee.employee_add');
 	}
-	Public function getEmployees()
+	Public function getAddEmployee()
 	{
-		
+		return view('employee.employee_add');
+	}
+	Public function postAddEmployee()
+	{
+		echo 'hjhjh';die;
 	}
 }
--- resources/views/employee/employee_add.blade.php
+++ resources/views/employee/employee_add.blade.php
@@ -17,7 +17,7 @@
     <section class="content">
 
       <div class="row">
-        <div class="col-md-12">
+        <div class="col-md-8" >
 
 
           <!-- About Me Box -->
@@ -27,31 +27,35 @@
             </div>
             <!-- /.box-header -->
             <div class="box-body">
-               <form role="form">
-              <div class="box-body">
+            <form role="form" action="{{ URL::to('employees/add-employee') }}" method="POST">
+            <div class="box-body">
                 <div class="form-group">
-                  <label for="exampleInputEmail1">Email address</label>
-                  <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
-                </div>
-                <div class="form-group">
-                  <label for="exampleInputPassword1">Password</label>
-                  <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                  <label for="FirstName">First Name</label>
+                  <input type="text" name='first_name' class="form-control" placeholder="Enter Name">
                 </div>
                 <div class="form-group">
-                  <label for="exampleInputFile">File input</label>
-                  <input type="file" id="exampleInputFile">
-
-                  <p class="help-block">Example block-level help text here.</p>
+                  <label for="LastName">Last Name</label>
+                  <input type="text" name='last_name' class="form-control"  placeholder="Enter Last Name">
                 </div>
-                <div class="checkbox">
-                  <label>
-                    <input type="checkbox"> Check me out
-                  </label>
+                <div class="form-group">
+                  <label for="contactno">Contact Number</label>
+                  <input type="text" name='contact_number' class="form-control" placeholder="Enter Contact Number">
                 </div>
-              </div>
-              <!-- /.box-body -->
-
-              <div class="box-footer">
+                <div class="form-group">
+                  <label for="contactno">Designation</label>
+                  <input type="text" name='designation' class="form-control" placeholder="Enter Designation">
+                </div>
+                <div class="form-group">
+                  <label for="Email">Email Id</label>
+                  <input type="text" name='email_id' class="form-control" placeholder="Enter Email Id">
+                </div>
+                <div class="form-group">
+                  <label for="Password">Password</label>
+                  <input type="password" name='password' class="form-control" placeholder="Enter Password">
+                </div>
+            </div>
+            <!-- /.box-body -->
+            <div class="box-footer">
                 <button type="submit" class="btn btn-primary">Submit</button>
               </div>
             </form>
